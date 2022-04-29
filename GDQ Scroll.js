05ad51eefb4f50c0fbc4de7fddbb098ff7d7c0c4
Tyler Shivers <jtshiv@users.noreply.github.com>
2022-04-29 14:34:45 -0400

    Create GDQ Scroll.js

---
+++ GDQ Scroll.js
@@ -0,0 +1,38 @@
+javascript: (function(){
+	var createScript = function(id, url, callback) {
+		if (document.getElementById('jqueryscript')) {
+			document.getElementById('jqueryscript').remove();
+		};
+		var s = document.createElement('script');
+		s.src = url;
+		s.id = id;
+		s.addEventListener('load', callback);
+		document.getElementsByTagName('head')[0].appendChild(s);
+	};
+	createScript("jquery", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js", function() {
+		var today = new Date;
+		/*var found;*/
+		for (elems of $('[found]')){
+			elems.removeAttribute('found');
+		};
+		for (line of $('.day-split')){
+			try{
+				var day = /(?:.*,\s.*\s)([0-9]+)(?:[a-z]+$)/g.exec(line.innerText)[1];
+				if (day==today.getDate()){
+					line.scrollIntoView({block: "center", inline: "nearest", behavior:"smooth"});
+					found = line;
+					found.setAttribute('found',1);
+					break;
+				};
+			} catch(e){
+				console.log(e);
+			};
+		};
+		$('[found=1]').nextAll('tr').attr('found',3);
+		$('[found=1]').nextAll('.day-split')[0].setAttribute('found',2);
+		$('[found=2]').nextAll('tr').removeAttr('found');
+		for (times of $('[found=3]').find('td.start-time')){
+			
+		};
+	});
+})();
