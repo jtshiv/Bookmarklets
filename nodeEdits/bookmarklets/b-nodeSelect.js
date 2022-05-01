3a101567e5731ade983caba3d5b612324aaf8c6d
Tyler Shivers <shiversjt@gmail.com>
2022-05-01 15:09:08 -0400

    With public, edited url. Modified createScript to have a set id and thats it. fn referenced with xmlhttprequest.

--- nodeEdits/bookmarklets/b-nodeSelect.js
+++ nodeEdits/bookmarklets/b-nodeSelect.js
@@ -1,31 +1,16 @@
 javascript: (function(){
-    var createScript = function(id, url, callback) {
+    var createScript = function() {
+        var id = "nodeSelect";
 		if (document.getElementById(id)) {
 			document.getElementById(id).remove();
 		};
 		var s = document.createElement('script');
-		s.src = url;
 		s.id = id;
-		s.addEventListener('load', callback);
+        s.innerHTML=this.responseText;
 		document.getElementsByTagName('head')[0].appendChild(s);
 	};
-	createScript("nodeSelect", "", function() {
-
-    });
+    var req = new XMLHttpRequest();
+    req.addEventListener("load",createScript);
+    req.open("GET","https://raw.githubusercontent.com/jtshiv/Bookmarklets/nodeSlim/nodeEdits/nodeSelect.js");
+    req.send();
 })();
-
-/*
-* html request
-* this didn't work bc the raw url has a token that expires.
-* I need to find an alternative
-*/
-
-/*
-function reqList(){
-    console.log(this.responseText);
-};
-var req = new XMLHttpRequest();
-req.addEventListener("load",reqList);
-req.open("GET","https://raw.githubusercontent.com/jtshiv/Bookmarklets/nodeSlim/nodeEdits/nodeSelect.js?token=GHSAT0AAAAAABUDKMWOMWAKL7HIWCVFKCQ4YTO2H3A");
-req.send();
-*/
