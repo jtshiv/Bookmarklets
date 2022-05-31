92de58ea8c498e2f84aa3cd4c8d2c31c345ae5da
Tyler <shiversjt@gmail.com>
2022-05-30 20:25:04 -0400

    fix: added selectors to target mobile views

--- Independent/rmListYt.js
+++ Independent/rmListYt.js
@@ -1,3 +1,14 @@
-javascript:(function(){for (elem of document.querySelectorAll('#video-title')){
-	    elem.href = elem.href.replace(/&list=.*$/,"");
-}})()
+javascript:(function(){
+	    /*desktop*/
+	    for (elem of document.querySelectorAll('#video-title')){
+		            elem.href = elem.href.replace(/&list=.*$/,"");
+		        }
+	    /*mobile image*/
+	    for (elem of document.querySelectorAll('.compact-media-item-image')){
+		            elem.href = elem.href.replace(/&list=.*$/,"");
+		        }
+	    /*mobile other*/
+	    for (elem of document.querySelectorAll('.compact-media-item-metadata-content')){
+		            elem.href = elem.href.replace(/&list=.*$/,"");
+		        }
+})()
