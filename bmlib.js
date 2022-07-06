75deccb9e0184cefae13e8006a2632c500a1f355
Tyler Shivers <shiversjt@gmail.com>
2022-07-06 12:01:35 -0400

    fix: changed vw to vh since modal is vertical

--- bmlib.js
+++ bmlib.js
@@ -248,7 +248,7 @@ function mainScript(){
         top: 0;
         width: 100%; /* Full width */
         height: 100%; /* Full height */
-        font-size: 3vw;
+        font-size: 3vh;
         overflow: auto; /* Enable scroll if needed */
         background-color: rgb(0,0,0); /* Fallback color */
         background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
